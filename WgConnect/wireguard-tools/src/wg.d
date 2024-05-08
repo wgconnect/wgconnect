72cdb46136670b23ab94b218299d833d43480463
wgconnect <wgconnect@proton.me>
2024-05-07 21:23:39 -0400

    Deleting binary files

--- WgConnect/wireguard-tools/src/wg.d
+++
@@ -1,8 +0,0 @@
-wg.o: wg.c subcommands.h jni-wg.h \
- /usr/lib/jvm/default-runtime/include/jni.h \
- /usr/lib/jvm/default-runtime/include/linux/jni_md.h version.h
-subcommands.h:
-jni-wg.h:
-/usr/lib/jvm/default-runtime/include/jni.h:
-/usr/lib/jvm/default-runtime/include/linux/jni_md.h:
-version.h:
